6672c6dc3c0b1a3b351556392ce454d421f54bd9
Vanessa Manalo <vanessa.m.manalo@gmail.com>
2023-08-24 16:55:00 -0800

    added coding solution

---
+++ 8kyu/you-cant-code-under-pressure.js
@@ -0,0 +1,11 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//My solution
+function doubleInteger(i) {
+    i *= 2
+    return i;
+  }
+
+  function doubleInteger(i) {
+    return i *= 2;
+  }
