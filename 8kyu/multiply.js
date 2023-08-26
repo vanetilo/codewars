93d0ad9f143f719122c8bcefe3d44473d39ea4bd
Vanessa Manalo <vanessa.m.manalo@gmail.com>
2023-08-25 17:55:00 -0800

    added codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,11 @@
+//This code does not execute properly. Try to figure out why.
+
+//Solution
+function multiply(a, b){
+    return a * b
+   }
+
+   function multiply(a, b){
+    let product = a * b
+    return product;
+  }
