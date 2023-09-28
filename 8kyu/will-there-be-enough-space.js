59debc13f4f3866c28ab6473d673349b0414a5a0
Vanessa Manalo <vanessa.m.manalo@gmail.com>
2023-09-27 17:04:00 -0700

    added new codewars challenge

---
+++ 8kyu/will-there-be-enough-space.js
@@ -0,0 +1,16 @@
+// Bob is working as a bus driver. However, he has become extremely popular amongst the city's residents. With so many passengers wanting to get aboard his bus, he sometimes has to face the problem of not enough space left on the bus! He wants you to write a simple program telling him if he will be able to fit all the passengers.
+
+// Task Overview:
+// You have to write a function that accepts three parameters:
+
+// cap is the amount of people the bus can hold excluding the driver.
+// on is the number of people on the bus excluding the driver.
+// wait is the number of people waiting to get on to the bus excluding the driver.
+
+
+
+//My solution
+
+function enough(cap, on, wait) {
+    return cap >= on + wait ? 0 : on + wait - cap
+   }
