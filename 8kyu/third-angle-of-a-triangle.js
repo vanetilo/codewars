5a5e97df4c2c239038ec8fbcf7e23662fcf3613d
Vanessa Manalo <vanessa.m.manalo@gmail.com>
2023-09-29 13:30:00 -0800

    challenge solution added

---
+++ 8kyu/third-angle-of-a-triangle.js
@@ -0,0 +1,8 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+//My solution 
+function otherAngle(a, b) {
+    return 180 - a - b;
+  }
