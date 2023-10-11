ffccc3bdb566e53a4232a111ee9b2d55fce2cb47
Vanessa Manalo <vanessa.m.manalo@gmail.com>
2023-10-11 14:55:00 -0800

    added codewars solution

--- 8kyu/training-js-1-create-your-first-js-function-and-print-hello-world.js
+++ 8kyu/training-js-1-create-your-first-js-function-and-print-hello-world.js
@@ -1,24 +1,3 @@
-// n JavaScript, your code is running in a function, let us step by step complete your first JS function.
-
-// Look at this example:
-
-//       --------keyword "function"
-//       |       ----your function name  
-//       |       |    ---if needed, some arguments will appear in parentheses
-//       |       |    |
-//     function myfunc(){  ---------your function code will starting with "{"
-//       //you should type your code here
-//     }----------------------------ending with "}"
-// If we want to print some to the screen, maybe we can use Document.write() in the web, or use alert() pop your message, but Codewars did not support these methods, we should use console.log() in your function. see this example:
-
-// function printWordToScreen(){
-//   var somewords="This is an example."
-//   console.log(somewords)
-// }
-// If we run this function, This is an example. will be seen on the screen. As you see, console.log() is an useful method. You will use it a lot!
-
-// Task
-// Please refer to two example above and write your first JS function.
 
 // mission 1:
 
